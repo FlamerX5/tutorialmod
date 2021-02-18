41420d47549316bafcf73ed7bc5f56c21d2787db
FlamerX5 <46610807+FlamerX5@users.noreply.github.com>
2021-02-17 22:19:46 -0500

    Add files via upload

---
+++ src/main/java/example/examplemod/init/ModItems.java
@@ -0,0 +1,12 @@
+package example.examplemod.init;
+
+import example.examplemod.ExampleMod;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(ExampleMod.MODID)
+public class ModItems {
+
+    public static final Item FIRST_ITEM = null;
+
+}
