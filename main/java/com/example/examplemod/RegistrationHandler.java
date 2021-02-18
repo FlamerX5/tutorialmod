bc017f5c9a9ad3d7d8926b9c268571a4af80ba73
FlamerX5 <46610807+FlamerX5@users.noreply.github.com>
2021-02-17 22:13:01 -0500

    Add files via upload

---
+++ main/java/com/example/examplemod/RegistrationHandler.java
@@ -0,0 +1,22 @@
+package com.example.examplemod;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber(modid = ExampleMod.MODID)
+public class RegistrationHandler {
+
+    @SubscribeEvent
+    public static void registerItems(Register<Item> event) {
+        final Item[] items = {
+                new Item().setRegistryName(ExampleMod.MODID, "first_item").setUnlocalizedName(ExampleMod.MODID + "." + "first_item").setCreativeTab(CreativeTabs.MISC)
+        };
+
+        event.getRegistry().registerAll(items);
+    }
+
+}
